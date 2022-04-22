5eda56c5e0ad9dcc6e2b515c6d76220feb879d81
MrWilliamA <will.archer@hotmail.com>
2022-04-22 14:35:26 +1000

    added iteractivity to keyboard and expanded funcitonality to all rows

--- src/index.js
+++ src/index.js
@@ -6,12 +6,13 @@
 // install eslint and clean things up
 
 
-// loop through each input and save variable
-// when 'guess' btn is clicked a function compares the two words one array element at a time, if true = color the div, if false skip. set all used inputs to disabled.
-// if word is write, disable all rows and alert(?) score i.e. number of rows.
+
 // for each letter guessed, mark of that letter form keyboard
 // if all guesses wrong reveal the word at the bottom
-
+// if enter is pressed, move focus to next row
+// count how many rows are used and save it as score
+// append score to dom
+// make keyboard clickable? ore remove pointer events if no time
 
 //update trello account
 //make a good readme. link to trello account 
@@ -76,7 +77,7 @@ const options = {
 //=======//
 
 const getRandomWord = async (word) => {
-    fetch('https://wordsapiv1.p.rapidapi.com/words/?letters=5&random=true', options)
+    fetch('https://wordsapiv1.p.rapidapi.com/words/?letters=5&random=true&letterPattern=[^\-\_]', options)
 	.then(response => response.json())
 	.then(response => {
         randomWord = response.word.replace(/[^a-zA-Z0-9 ]/g, '').toUpperCase().split("");
@@ -88,15 +89,22 @@ const getRandomWord = async (word) => {
 getRandomWord();
 
 
-function moveOnMax(field, nextFieldID) { 
+function moveOnMax(nextFieldID) { 
     const maxLength = 1;
     let nextId =  parseInt(nextFieldID.match(/\d+/)[0], 10) + 1;
-    
+
     let nextInput = document.getElementById(`input${nextId}`);
     
-    if (field.length >= maxLength) { 
-        nextInput.focus(); 
+    let excludedInputs = ['input6', 'input11', 'input16', 'input21', 'input26',]
+    
+    let inputsCompared = (nextFieldID != "input30") ? excludedInputs.includes(nextInput.id) : "input6";
+
+    if (maxLength => 1 || !inputsCompared) { 
+        if(!inputsCompared) {
+            nextInput.focus(); 
+        }
     } 
+
 }  
 
 function rightAsnwer(thisInput) {
@@ -104,15 +112,38 @@ function rightAsnwer(thisInput) {
     const finished =  document.getElementById("finishedBox").classList.add("show");
 }
 
-function wrongAnswer(guessArray, randomWord, thisInput) {
+function wrongAnswer(guessArray, randomWord, rows) {
+    
+    const inputColour = rows.children;
 
     for(let i = 0; i < 5; i++ ) {
         if(guessArray[i] === randomWord[i]) {
-            inputList[i].style.backgroundColor = "#057805";
+            inputColour[i].style.backgroundColor = "rgb(5 120 5)";
+            console.log(guessArray[i])
+
+
+            // get keyboard element b innertext?
+            // const keyboard = document.querySelectorAll('.keyboardLine span').innerHTML;
+
+            const keyboard = [].map.call(document.querySelectorAll('.keyboardLine span'), function(el) {
+                if(el.innerHTML == guessArray[i]){
+                    el.classList.add("guessedRight");;
+                }
+            })
+            
+            
+
         } else {
-            inputList[i].style.backgroundColor = "#a11919";
+            inputColour[i].style.backgroundColor = "rgb(161 25 25)";
+            const keyboard = [].map.call(document.querySelectorAll('.keyboardLine span'), function(el) {
+                if(el.innerHTML == guessArray[i]){
+                    el.classList.add("guessedWrong");;
+                }
+            })
+            
         }
     }
+
 }
 
 const checkAnswer = (thisInput) => {
@@ -134,7 +165,7 @@ const checkAnswer = (thisInput) => {
         if(guess === answer) {
             rightAsnwer(thisInput);
         } else {
-            wrongAnswer(guessArray, randomWord, thisInput);
+            wrongAnswer(guessArray, randomWord, rows);
         }
     } 
  }
@@ -146,19 +177,48 @@ for (let i = 0 ; i < inputList.length; i++) {
         let thisInputValue = event.target.value;
         let nextInput = event.target.getAttribute('id');
 
-        moveOnMax(thisInputValue, nextInput);
-        // change this so it only happens when enter is pressed, or guess button clicked
+        moveOnMax(nextInput);
+
         if (event.key === 'Enter') {
             checkAnswer(thisInput);
-            
+                   
+        const rowsOff = document.querySelectorAll(".inputRow");
+        let rowDivided = [ ...rowsOff ];
 
-            const rows = thisInput.parentElement.parentElement;
-            console.log(rows)
-            //if this is the last input, reveal next row and freeze current row with: .style.pointerEvents = "none";
-            if(thisInput == false) {
-                console.log('last one')
-            }
-        }
-    });
+        function disableEnableRows() {
+            rowDivided.forEach(row => {
+            row.classList.add("disabled");
+        });
 
 };
+
+            switch(thisInput.id) {
+                case "input5":
+                    disableEnableRows();
+                    rowDivided[1].classList.remove("disabled")
+                    break;
+                case "input10":
+                    disableEnableRows();
+                    rowDivided[2].classList.remove("disabled")
+                    break;
+                case "input15":
+                    disableEnableRows();
+                    rowDivided[3].classList.remove("disabled")
+                    break;
+                case "input20":
+                    disableEnableRows();
+                    rowDivided[4].classList.remove("disabled")
+                    break;
+                case "input25":
+                    disableEnableRows();
+                    rowDivided[5].classList.remove("disabled")
+                    break;
+                case "input30":
+                    disableEnableRows();
+                    rightAsnwer();
+                    break;
+            }
+        };
+    });
+}
+
